19f7066263f8caabb764a784ef6cfdb5d546f8fc
joe-qodes <jquao04@gmail.com>
2025-11-21 13:00:38 +0000

    Created arithmetic_operations.py file

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,6 +2,6 @@ size = int(input("Enter the size of the pattern: "))
 row = 0
 while row < size:
     for i in range(size):
-        print("*", end="")
+        print(row * "*", end="")
     print()
     row += 1
---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,11 @@
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            raise ValueError('Division by 0 is invalid')
+        return num1 / num2
